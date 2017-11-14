2c16b8e5b1858daa546c454d0ed85c69fa8a48b2
Kristaps Grinbergs <kristaps@qminderapp.com>
2017-11-14 12:30:52 +0200

    Improved ticket comparable

--- TicketComparable.playground/Contents.swift
+++ TicketComparable.playground/Contents.swift
@@ -2,53 +2,170 @@
 
 import Foundation
 
-struct Ticket {
-  var created: Date
+enum Status: String {
+  case new = "NEW"
+  case called = "CALLED"
+  case cancelled = "CANCELLED"
+  case cancelledByClerk = "CANCELLED_BY_CLERK"
+  case noShow = "NOSHOW"
+  case served = "SERVED"
 }
 
-let dateISO8601MillisecondsFormatter = DateFormatter()
-dateISO8601MillisecondsFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+public struct Created {
+  public var date: Date?
+}
+
+extension Created: Comparable {
+  public static func ==(lhs: Created, rhs: Created) -> Bool {
+    return lhs.date == rhs.date
+  }
+  
+  public static func < (lhs: Created, rhs: Created) -> Bool {
+    return lhs.date! < rhs.date!
+  }
+}
+
+public struct Called: Codable {
+  public var date: Date?
+  public var desk: Int?
+  public var caller: Int?
+}
+
+public struct Served: Codable {
+  public let date: Date?
+}
+
+extension Served: Comparable {
+  public static func ==(lhs: Served, rhs: Served) -> Bool {
+    return lhs.date == rhs.date
+  }
+  
+  public static func < (lhs: Served, rhs: Served) -> Bool {
+    return lhs.date! < rhs.date!
+  }
+}
+
+
+struct Ticket {
+  
+  var created: Created
+  var status: Status
+  
+  var called: Called?
+  var served: Served?
+  var orderAfter: Date?
+}
+
+let dateFormat = DateFormatter()
+dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
 
 let tickets = [
-  Ticket(created: dateISO8601MillisecondsFormatter.date(from: "2017-02-06T12:35:29.123Z")!),
-  Ticket(created: dateISO8601MillisecondsFormatter.date(from: "2017-02-06T12:35:29.124Z")!),
-  Ticket(created: dateISO8601MillisecondsFormatter.date(from: "2017-02-06T12:35:29.125Z")!),
-  Ticket(created: dateISO8601MillisecondsFormatter.date(from: "2017-02-06T12:35:29.126Z")!),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:35:29.123Z")!), status: .new, called: nil, served: nil, orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:35:29.124Z")!), status: .called, called: nil, served: nil, orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:35:29.125Z")!), status: .new, called: nil, served: nil, orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:35:29.126Z")!), status: .new, called: nil, served: nil, orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:35:29.146Z")!), status: .noShow, called: nil, served: Served(date: dateFormat.date(from: "2017-02-06T12:39:29.146Z")!), orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:32:29.124Z")!), status: .called, called: nil, served: nil, orderAfter: nil),
+  Ticket(created: Created(date: dateFormat.date(from: "2017-02-06T12:38:29.146Z")!), status: .noShow, called: nil, served: Served(date: dateFormat.date(from: "2017-02-06T12:58:29.146Z")!), orderAfter: nil),
 ]
 
 extension Ticket: Comparable {
   
   // Equatable
   public static func ==(lhs: Ticket, rhs: Ticket) -> Bool {
-    switch lhs.created.compare(rhs.created) {
-    case .orderedAscending:
-      return false
-    case .orderedDescending:
-      return false
-    case .orderedSame:
-      return true
-    }
+    
+    guard lhs.status == rhs.status else { return false }
+    guard lhs.created == rhs.created else { return false }
+    
+    return true
   }
   
   // Comparable
   public static func <(lhs: Ticket, rhs: Ticket) -> Bool {
-    switch lhs.created.compare(rhs.created) {
-    case .orderedAscending:
+
+    switch (lhs.status, rhs.status) {
+    case (.noShow, .noShow):
+      return lhs.created < rhs.created
+    case (.noShow, .called):
       return true
-    case .orderedDescending:
-      return false
-    case .orderedSame:
+    case (.noShow, .new):
+      return true
+    case (.called, .called):
+      return lhs.created < rhs.created
+    case (.called, .new):
+      return true
+    case (.new, .new):
+      return lhs.created < rhs.created
+    default:
       return false
     }
+    
+    /**
+     
+     NOSHOW
+     * served <
+     CALLED
+     * called date <
+     NEW:
+     * order after <
+     * created <
+     
+    */
+    
+//    // If ticket is called
+//    if lhs.status == .noShow {
+//      guard let lhsServedDate = lhs.served?.date else {
+//        return false
+//      }
+//
+//      // If other one is called then compare called date
+//      if rhs.status == .noShow {
+//
+//        guard let rhsServedDate = rhs.served?.date else {
+//          return true
+//        }
+//
+//        return lhsServedDate.compare(rhsServedDate) == ComparisonResult.orderedDescending
+//      } else {
+//        // CALLED > NEW ticet
+//        return true
+//      }
+//    } else if lhs.status == .called {
+//
+//      guard let lhsCalledDate = lhs.called?.date else {
+//        return false
+//      }
+//
+//      // If other one is called then compare called date
+//      if rhs.status == .called {
+//
+//        guard let rhsCalledDate = rhs.called?.date else {
+//          return true
+//        }
+//
+//        return lhsCalledDate.compare(rhsCalledDate) == ComparisonResult.orderedDescending
+//      } else {
+//        // CALLED > NEW ticet
+//        return true
+//      }
+//    } else {
+//
+//      // compare order after dates with nanosecond
+//
+//      let orderAfter0 = lhs.orderAfter ?? (lhs.created?.date)!
+//      let orderAfter1 = rhs.orderAfter ?? (rhs.created?.date)!
+//
+//      return orderAfter0.compare(orderAfter1) == ComparisonResult.orderedAscending
+//    }
   }
 }
 
 extension Ticket: CustomStringConvertible {
   var description: String {
-    let dateISO8601MillisecondsFormatter = DateFormatter()
-    dateISO8601MillisecondsFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    let dateFormat = DateFormatter()
+    dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
     
-    return dateISO8601MillisecondsFormatter.string(from: created)
+    return status.rawValue + " " + dateFormat.string(from: created.date!)
   }
 }
 
